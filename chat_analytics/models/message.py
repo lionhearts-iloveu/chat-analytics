a68087e0ec88281ccf91b85ed792678ac205be86
Antoine Cayphas <a.koos.cayphas@gmail.com>
2021-04-05 15:56:01 +0200

    add TODO

--- chat_analytics/models/message.py
+++ chat_analytics/models/message.py
@@ -4,5 +4,6 @@ from datetime import datetime
 class Message:
     def __init__(self, sender: str, content: str, data_ms: datetime):
         self.sender = sender
+        # TODO normalize content
         self.content = content
         self.date_ms = data_ms
